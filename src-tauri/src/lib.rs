8ae3998d3b39f0eb293655b6eeffe6ed435c0112
XingGao <xinggaoya@qq.com>
2026-06-18 12:11:57 +0800

    feat: add tauri-plugin-notification dependency and registration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -112,6 +112,7 @@ pub fn run() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_notification::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .level(tauri_plugin_log::log::LevelFilter::Info)
